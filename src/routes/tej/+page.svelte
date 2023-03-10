70d93ee5d33db5a5fdb3b931db9f4b322853a601
valid <valid@zgen.hu>
2023-03-10 02:39:44 +0100

    add content & improve styling

--- src/routes/tej/+page.svelte
+++ src/routes/tej/+page.svelte
@@ -1,26 +1,36 @@
 <script>
 	import banner from '$lib/images/banner.jpg';
-	import logo from '$lib/images/logo.jpg';
+	import poap from '$lib/images/poap.gif';
 </script>
 
 <svelte:head>
 	<title>DOMINGO Tejfakasztó Debütkoncert POAP</title>
 </svelte:head>
 
-<section class="bg-[#423f3f] md:rounded-2xl md:mt-14 overflow-hidden">
+<section class="bg-[#232323] md:rounded-2xl md:mt-14 overflow-hidden shadow-md">
 	<img src={banner} />
-	<div class="relative w-full flex justify-center items-center mb-14">
-		<img src={logo} class="rounded-full absolute w-36  border-4 border-[#423f3f]" />
+	<div class="relative w-full flex justify-center items-center mb-16">
+		<img src={poap} class="rounded-full absolute w-36  border-4 border-[#423f3f] shadow-md" />
 	</div>
-	<div class="p-6 md:p-8 w-full">
-		<h1 class="text-center font-bold text-2xl mb-6">Tejfakasztó Debütkoncert POAP</h1>
-		<p class="text-center mb-10">
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-			been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-			galley of type and scrambled it to make a type specimen book. It has survived not only five
-			centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+	<div class="p-5 md:p-8 w-full">
+		<h1 class="text-center font-bold text-2xl mb-6 font-[Anton] text-[#9D7B77]">
+			Debütkoncert POAP
+		</h1>
+		<p class="text-center mb-4 font-normal text-[#E1D5CA] font-[Manrope] ">
+			Ez itt egy POAP NFT, egy digitális plecsni arról, hogy részt vettél az első koncertünkön. Ha
+			megszerzed, később <b> mindenféle jóságokhoz férhetsz majd hozzá általa!</b>
 		</p>
-		<form method="POST" class="inline-block flex items-end">
+		<p class="text-center mb-4 font-normal text-[#E1D5CA] font-[Manrope]">
+			Például hamarabb hallhatod majd az új anyagainkat, olcsóbban vehetsz jegyet a koncertjeinkre,
+			meg amit még kitalálunk. Nem mellesleg nagyon menő, és később fizikai kitűző formájában is
+			birtokolhatod!
+		</p>
+		<p class="text-center mb-10 font-normal text-[#E1D5CA] font-[Manrope]">
+			Most csak az email címedre van hozzá szükség, és hogy a következő oldalon a
+			<i> Mint </i>
+			gombra nyomj, amit <b>holnap estig tehetsz meg</b>. A továbbiakról szólni fogunk 🖖🏻
+		</p>
+		<form method="POST" class="inline-block flex items-end pb-8">
 			<label class="w-full"
 				><!-- Add meg az emailed! -->
 				<input
@@ -31,7 +41,12 @@
 					class="rounded-lg block h-12  mt-1 w-full bg-[#282727] border-[#3f3e3e]"
 				/>
 			</label>
-			<button class="rounded-lg px-5 h-12 bg-[#927470] ml-2">Mehet</button>
+			<button class="rounded-lg px-5 h-12 bg-[#927470] ml-2 font-[Anton] text-[#E1D5CA]"
+				>Mehet</button
+			>
 		</form>
 	</div>
 </section>
+<p class="text-center  font-normal text-[#5c5c5c] font-[Manrope] py-8">
+	Instagram / Facebook: @domingozenekar
+</p>
